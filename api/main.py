bc60c9ebc13230913baa549ff6204d59660fed83
Lucas Gauvain <lucasgauvain@gmail.com>
2022-02-08 14:40:46 +0100

    example crud user incomplet

--- api/main.py
+++ api/main.py
@@ -1,27 +1,57 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+import pymongo
+from bson import ObjectId
 from typing import Optional
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
 app = FastAPI()
+client = pymongo.MongoClient(
+    "mongodb+srv://rob:rob123456@cluster0.hv6ea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
 
 
-class Item(BaseModel):
+class PyObjectId(ObjectId):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
+
+class User(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
     name: str
-    price: float
-    is_offer: Optional[bool] = None
+    username: str
+    email: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get('/users')
+async def list_users():
+    users = []
+    for user in db.users.find():
+        users.append(User(**user))
+    return {'users': users}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.post('/users')
+async def create_user(user: User):
+    if hasattr(user, 'id'):
+        delattr(user, 'id')
+    ret = db.users.insert_one(user.dict(by_alias=True))
+    user.id = ret.inserted_id
+    return {'user': user}
