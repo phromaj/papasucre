957f569470bfc04259cc9d00a1d7b2c0415d48a5
robLittiere <robinl.95@orange.fr>
2022-02-08 15:28:25 +0100

    Add User and pyObjectId models

--- api/main.py
+++ api/main.py
@@ -3,53 +3,23 @@ from pydantic import BaseModel, Field
 import pymongo
 from bson import ObjectId
 from typing import Optional
-
+from models import user
 app = FastAPI()
 client = pymongo.MongoClient(
     "mongodb+srv://rob:rob123456@cluster0.hv6ea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.test
 
 
-class PyObjectId(ObjectId):
-
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError('Invalid objectid')
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type='string')
-
-
-class User(BaseModel):
-    id: Optional[PyObjectId] = Field(alias='_id')
-    name: str
-    username: str
-    email: str
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {
-            ObjectId: str
-        }
-
-
 @app.get('/users')
 async def list_users():
     users = []
-    for user in db.users.find():
-        users.append(User(**user))
+    for item in db.users.find():
+        users.append(user.UserModel(**item))
     return {'users': users}
 
 
 @app.post('/users')
-async def create_user(user: User):
+async def create_user(user: user.UserModel):
     if hasattr(user, 'id'):
         delattr(user, 'id')
     ret = db.users.insert_one(user.dict(by_alias=True))
---
+++ api/models/user.py
@@ -0,0 +1,17 @@
+from bson import ObjectId
+from typing import Optional
+from pydantic import BaseModel, Field
+import pyObjectId
+
+
+class UserModel(BaseModel):
+    id: Optional[pyObjectId.PyObjectId] = Field(alias='_id')
+    name: str
+    username: str
+    email: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
